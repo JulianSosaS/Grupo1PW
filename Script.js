5c6d88a68b4c47b161073efe2185678cb33a05ce
JulianSosaS <julianss1221@gmail.com>
2024-08-25 14:35:31 -0500

    Formulario V4

--- Script.js
+++ Script.js
@@ -10,11 +10,13 @@ document.getElementById('cuentoForm').addEventListener('submit', function(event)
     const hobby = document.getElementById('hobby').value;
 
     // Genera el cuento
-    const cuento = `Había una vez un joven llamado ${nombres}, conocido por sus amigos como "${apodo}". 
-    Tenía un hermoso cabello de color ${colorCabello} y ojos ${colorOjos}. 
-    A la edad de ${edad} años, ${nombres} disfrutaba de su hobby favorito: ${hobby}. 
-    Un día, decidió aventurarse en una mágica travesía que cambiaría su vida para siempre.`;
+    const cuento = `${nombres}, conocido cariñosamente por sus amigos como "${apodo}", tenía un cabello de color ${colorCabello} que siempre llamaba la atención, y unos ojos ${colorOjos} que parecían esconder secretos. 
+    A sus ${edad} años, ${nombres} era una persona apasionada por ${hobby}. Un día, mientras practicaba su hobby, encontró un antiguo y polvoriento libro que hablaba de un tesoro oculto.
 
+    Intrigado, ${nombres} decidió seguir las pistas del libro, y sin dudarlo, emprendió un viaje lleno de desafíos. En su travesía, se encontró con criaturas mágicas y lugares increíbles. 
+    Cada paso que daba lo acercaba más a descubrir un mundo que jamás había imaginado. Y aunque el camino estaba lleno de peligros, su espíritu aventurero y su amor por ${hobby} le dieron la fuerza para continuar.
+
+    Al final, ${nombres} aprendió que el verdadero tesoro no era lo que estaba buscando, sino las experiencias y amistades que hizo en el camino.`;
     // Muestra el cuento en la página
     document.getElementById('cuento').innerText = cuento;
 });
